59fda6544d927aecc5298be5c761797dea4f3226
teolessonplanet <teo@lessonplanet.com>
2019-01-29 18:27:13 +0200

    upgrade free member - rebase

--- src/main/java/com/lessonplanet/pages/SubscriptionEmailModal.java
+++ src/main/java/com/lessonplanet/pages/SubscriptionEmailModal.java
@@ -7,7 +7,6 @@ public class SubscriptionEmailModal extends LpUiBasePage {
     private static final String SUBSCRIPTION_MODAL = "#subscription-modal-popup";
     private static final String SUBSCRIPTION_MODAL_TEXT = "[class*='modal-body']";
     private static final String CLOSE_BUTTON = "[class*='close']";
-    private static final String SUBSCRIPTION_MODAL_ID = "#subscription-modal-popup";
 
     public SubscriptionEmailModal(WebDriver driver) {
         super(driver);
@@ -23,9 +22,5 @@ public class SubscriptionEmailModal extends LpUiBasePage {
 
     public void clickOnCloseButton() {
         clickElement(findElements(CLOSE_BUTTON), 0);
-    }
-
-    public void waitForModal(){
-        waitForBootstrapModalToBeVisible(SUBSCRIPTION_MODAL_ID);
     }
 }
