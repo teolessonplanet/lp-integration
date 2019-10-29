f85a980aa7f57a36da5a3a056a6350f90a42cd40
Florin Nechita <florin.nechita@softvision.ro>
2019-10-29 17:59:14 +0200

    sl_directory

---
+++ src/test/java/RSL_DirectoryMainPageTest.java
@@ -0,0 +1,77 @@
+import com.lessonplanet.pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import util.TestData;
+
+public class RSL_DirectoryMainPageTest extends BaseTest {
+
+    private LoginPage loginPage;
+    private DirectoryMainPageTest directoryMainPageTest;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        loginPage = new LoginPage(webDriver);
+        directoryMainPageTest = new DirectoryMainPageTest();
+    }
+
+    public void initTest(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        beforeMethod();
+    }
+
+    @Test(description = "Regular SL - Directory Page - Main Page - lessonp-5178: Page UI")
+    public void testLessonp_5178() {
+        testPageUi(TestData.VALID_EMAIL_RSL_SBCEO);
+    }
+
+    @Test(description = "Regular SL - Directory Page - Main Page - lessonp-5179: Thumbnails")
+    public void testLessonp_5179() {
+        testThumbnails(TestData.VALID_EMAIL_RSL_SBCEO);
+    }
+
+    @Test(description = "Regular SL - Directory Page - Main Page - lessonp-5180: Links")
+    public void testLessonp_5180() {
+        testLinks(TestData.VALID_EMAIL_RSL_SBCEO);
+    }
+
+    @Test(description = "Regular SL - Directory Page - Main Page - lessonp-5181: Show All button")
+    public void testLessonp_5181() {
+        testShowAllButton(TestData.VALID_EMAIL_RSL_SBCEO);
+    }
+
+    @Test(description = "Regular SL - Directory Page - Main Page - lessonp-5182: Categories modal")
+    public void testLessonp_5182() {
+        testCategoriesModal(TestData.VALID_EMAIL_RSL_SBCEO);
+    }
+
+    private void initDirectoryTestAndLogin(String account) {
+        loginPage.performLogin(account, TestData.VALID_PASSWORD);
+        directoryMainPageTest.initTest(webDriver);
+    }
+
+    protected void testPageUi(String account) {
+        initDirectoryTestAndLogin(account);
+        directoryMainPageTest.testPageUi();
+    }
+
+    protected void testThumbnails(String account) {
+        initDirectoryTestAndLogin(account);
+        directoryMainPageTest.testThumbnails();
+    }
+
+    protected void testLinks(String account) {
+        initDirectoryTestAndLogin(account);
+        directoryMainPageTest.testLinks();
+    }
+
+    protected void testShowAllButton(String account) {
+        initDirectoryTestAndLogin(account);
+        directoryMainPageTest.testShowAllButton();
+    }
+
+    protected void testCategoriesModal(String account) {
+        initDirectoryTestAndLogin(account);
+        directoryMainPageTest.testCategoriesModal();
+    }
+}
