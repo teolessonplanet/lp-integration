236b8bfe311e033d10f4c952207ec5414ed44ae7
Florin Nechita <florin.nechita@softvision.ro>
2020-09-04 11:19:48 +0300

    nested folders drag&drop custom function

--- src/main/java/com/lessonplanet/pages/LpUiBasePage.java
+++ src/main/java/com/lessonplanet/pages/LpUiBasePage.java
@@ -324,6 +324,18 @@ public class LpUiBasePage {
         waitForLoad();
     }
 
+    public void customdragAndDrop(WebElement element, WebElement target) {
+        waitForLoad();
+        try {
+            // https://gist.github.com/florentbr/60ef7cb8d9b1ae690cafc82aad52da73#file-drag-drop-js
+            String html5dnd = "function e(e,t,n,i){var r=a.createEvent('DragEvent');r.initMouseEvent(t,!0,!0,o,0,0,0,c,g,!1,!1,!1,!1,0,null),Object.defineProperty(r,'dataTransfer',{get:function(){return d}}),e.dispatchEvent(r),o.setTimeout(i,n)}var t=arguments[0],n=arguments[1],i=arguments[2]||0,r=arguments[3]||0;if(!t.draggable)throw new Error('Source element is not draggable.');var a=t.ownerDocument,o=a.defaultView,l=t.getBoundingClientRect(),u=n?n.getBoundingClientRect():l,c=l.left+(l.width>>1),g=l.top+(l.height>>1),s=u.left+(u.width>>1)+i,f=u.top+(u.height>>1)+r,d=Object.create(Object.prototype,{_items:{value:{}},effectAllowed:{value:'all',writable:!0},dropEffect:{value:'move',writable:!0},files:{get:function(){return this._items.Files}},types:{get:function(){return Object.keys(this._items)}},setData:{value:function(e,t){this._items[e]=t}},getData:{value:function(e){return this._items[e]}},clearData:{value:function(e){delete this._items[e]}},setDragImage:{value:function(e){}}});if(n=a.elementFromPoint(s,f),!n)throw new Error('The target element is not interactable and need to be scrolled into the view.');u=n.getBoundingClientRect(),e(t,'dragstart',101,function(){var i=n.getBoundingClientRect();c=i.left+s-u.left,g=i.top+f-u.top,e(n,'dragenter',1,function(){e(n,'dragover',101,function(){n=a.elementFromPoint(c,g),e(n,'drop',1,function(){e(t,'dragend',1,callback)})})})})";
+          javascriptExecutor.executeScript(html5dnd, element, target);
+        } catch (Exception e) {
+            dragAndDrop(element, target);
+        }
+        waitForLoad();
+    }
+
     public void hoverOverElement(WebElement element) {
         waitForLoad();
         (new Actions(driver)).moveToElement(element).build().perform();
