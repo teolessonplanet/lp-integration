48b08dbf6713f6f41bb4981445e72f8bf2ea3bfc
Teodora Vermesan <teo@lessonplanet.com>
2021-02-04 11:20:30 +0200

    split cobb tests

---
+++ src/test/java/Cobb_HeaderTest.java
@@ -0,0 +1,29 @@
+import com.lessonplanet.pages.LoginPage;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import util.TestData;
+
+public class Cobb_HeaderTest extends BaseTest {
+    private LoginPage loginPage;
+    private HeaderTest headerTest;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        loginPage = new LoginPage(webDriver);
+        headerTest = new HeaderTest();
+    }
+
+    @Test(description = "Cobb - Header - lessonp-5894: Header buttons")
+    public void testLessonp_5894() {
+        headerTest.initTest(webDriver);
+        loginPage.performLogin(TestData.VALID_EMAIL_CSL_COBB, TestData.VALID_PASSWORD);
+        headerTest.testHeaderButtons(TestData.VALID_EMAIL_CSL_COBB);
+    }
+
+    @Test(description = "Cobb - Header - lessonp-5383: Search box")
+    public void testLessonp_5383() {
+        headerTest.initTest(webDriver);
+        loginPage.performLogin(TestData.VALID_EMAIL_CSL_COBB, TestData.VALID_PASSWORD);
+        headerTest.testSearchBoxKeywordFunctionality(TestData.VALID_EMAIL_CSL_COBB);
+    }
+}
