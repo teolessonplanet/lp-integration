357348968f34cf25083fafd73ec72c233b9eb188
Teodora Vermesan <teo@lessonplanet.com>
2021-12-17 13:34:52 +0200

    fix failed tests rrp - limited access review

--- src/main/java/com/lessonplanet/pages/LimitedResourceAccessModal.java
+++ src/main/java/com/lessonplanet/pages/LimitedResourceAccessModal.java
@@ -4,10 +4,10 @@ import org.openqa.selenium.WebDriver;
 
 public class LimitedResourceAccessModal extends LpUiBasePage {
 
-    private static final String LIMITED_RESOURCE_ACCESS_MODAL = "#limited-resources-access";
-    private static final String LIMITED_RESOURCE_ACCESS_MODAL_BODY_TEXT = "[class='modal-body'] [class='mb20']";
+    private static final String LIMITED_RESOURCE_ACCESS_MODAL = "#limited-resource-access-modal";
+    private static final String LIMITED_RESOURCE_ACCESS_MODAL_BODY_TEXT = "#limited-resource-access-modal [class='modal-body']";
     private static final String LIMITED_RESOURCE_ACCESS_MODAL_TITLE_TEXT = "[class='modal-header'] h3";
-    private static final String SAVE_BUTTON = "#limited-resources-access [class='modal-footer'] #save-with-limited-items";
+    private static final String SAVE_BUTTON = "#limited-resource-access-modal [class='modal-footer'] [class='btn btn-primary']";
 
     public LimitedResourceAccessModal(WebDriver driver) {
         super(driver);
